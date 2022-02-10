e4bfb1d798b7d59372239a2af6be16d4093a9121
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-02-09 20:35:52 -0600

    Create sussy.js

---
+++ unblkins/sussy.js
@@ -0,0 +1,9 @@
+var NewThing = document.createElement("video");
+
+NewThing.src = "https://www.blzby1.github.io/unblkins/sussy.mp4"
+NewThing.style = "width:100%;height:100%;border-style:none;"
+NewThing.autoplay = true
+
+document.write(NewThing)
+
+// very, very sus. use at your own risk
