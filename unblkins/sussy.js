f167852d797987d6fdef09eb9f530f9b747c0039
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-02-09 21:07:09 -0600

    Update sussy.js

--- unblkins/sussy.js
+++ unblkins/sussy.js
@@ -4,6 +4,6 @@ NewThing.src = "https://blzby1.github.io/unblkins/sussy.mp4"
 NewThing.style = "width:100%;height:100%;border-style:none;"
 NewThing.autoplay = true
 
-document.write(NewThing)
+document.write("NewThing")
 
 // very, very sus. use at your own risk
