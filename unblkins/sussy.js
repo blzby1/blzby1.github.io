57e66ba243b975ce8ed4e4a8ec41c57a47382c3b
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-02-09 21:02:59 -0600

    Update sussy.js

--- unblkins/sussy.js
+++ unblkins/sussy.js
@@ -1,6 +1,6 @@
 var NewThing = document.createElement("video");
 
-NewThing.src = "https://www.blzby1.github.io/unblkins/sussy.mp4"
+NewThing.src = "https://blzby1.github.io/unblkins/sussy.mp4"
 NewThing.style = "width:100%;height:100%;border-style:none;"
 NewThing.autoplay = true
 
